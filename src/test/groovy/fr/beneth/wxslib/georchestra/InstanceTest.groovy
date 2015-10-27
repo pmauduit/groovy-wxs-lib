ae59efaf7eb018ad00d64894c2ea047dd099c39f
Pierre Mauduit <pierre.mauduit@gmail.com>
2015-10-27 19:41:53 +0100

    fixing test

--- src/test/groovy/fr/beneth/wxslib/georchestra/InstanceTest.groovy
+++ src/test/groovy/fr/beneth/wxslib/georchestra/InstanceTest.groovy
@@ -3,6 +3,8 @@ package fr.beneth.wxslib.georchestra
 import fr.beneth.wxslib.georchestra.Instance
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by pmauduit on 10/27/15.
  */
