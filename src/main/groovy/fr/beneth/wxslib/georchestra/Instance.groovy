fef7772cc87eed08a47770d780ac62fb72e53563
Pierre Mauduit <pierre.mauduit@gmail.com>
2015-10-27 19:37:27 +0100

    adding some code to get the geOr instances

---
+++ src/main/groovy/fr/beneth/wxslib/georchestra/Instance.groovy
@@ -0,0 +1,29 @@
+package fr.beneth.wxslib.georchestra;
+
+/**
+ * Created by pmauduit on 10/27/15.
+ */
+public class Instance {
+    private static String SDI_LIST_ENDPOINT = "http://sdi.georchestra.org/geoserver/wfs?request=getfeature&service=wfs&typeName=geor:sdi"
+
+    String title, url, logo_url
+    boolean isPublic, isInProduction
+
+    static ArrayList<Instance> loadGeorchestraInstances() {
+        def xmlData = new XmlSlurper().parse(SDI_LIST_ENDPOINT)
+        xmlData.declareNamespace([geor: "http://sdi.georchestra.org/geor"])
+        def instances = new ArrayList<Instance>()
+        xmlData.children().each { wfsMember ->
+            def curInst =  new Instance()
+
+            curInst.title = wfsMember.'geor:sdi'.'geor:title'
+            curInst.url = wfsMember.'geor:sdi'.'geor:url'
+            curInst.logo_url = wfsMember.'geor:sdi'.'geor:logo_url'
+            curInst.isPublic = wfsMember.'geor:sdi'.'geor:is_public' == "true"
+            curInst.isInProduction = wfsMember.'geor:sdi'.'geor:is_production' == "true"
+            instances << curInst
+        }
+        return instances
+    }
+
+}
---
+++ src/test/groovy/fr/beneth/wxslib/georchestra/InstanceTest.groovy
@@ -0,0 +1,16 @@
+package fr.beneth.wxslib.georchestra
+
+import fr.beneth.wxslib.georchestra.Instance
+import org.junit.Test;
+
+/**
+ * Created by pmauduit on 10/27/15.
+ */
+public class InstanceTest {
+
+    @Test
+    void testGetInstances() {
+        def instances = Instance.loadGeorchestraInstances()
+        assertTrue(instances.size() > 0)
+    }
+}
