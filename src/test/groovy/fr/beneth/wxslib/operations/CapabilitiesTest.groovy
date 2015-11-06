3f1de7a191a35d04585494a96964c50218713dc4
Pierre Mauduit <pierre.mauduit@gmail.com>
2015-11-06 20:22:12 +0100

    Reindent - no func changes

--- src/test/groovy/fr/beneth/wxslib/operations/CapabilitiesTest.groovy
+++ src/test/groovy/fr/beneth/wxslib/operations/CapabilitiesTest.groovy
@@ -10,45 +10,45 @@ import static org.junit.Assert.assertFalse
 
 
 class CapabilitiesTest {
-	@Test
-	void testSdiGeorchestraOrgWmsCapabilities() {
-		def getCapResp = this.getClass().getResource("sdi-geor.getcap.xml")
-		def capInfo = Capabilities.mapFromDocument(getCapResp.toString())
-		
-		assertTrue(capInfo.name == "WMS")
-		assertTrue(capInfo._abstract ==~ /.*modular and opensource spatial data infrastructure.*/)
-		assertTrue(capInfo.keywords.size == 2)
-		assertTrue(capInfo.layers.size == 1)
-		assertTrue(capInfo.title == "geOrchestra Web Map Service")
-		assertTrue(capInfo.contactEmail == "psc@georchestra.org")
-		assertTrue(capInfo.contact == "geOrchestra PSC")
-		
-		Layer fo = capInfo.findLayerByName("pmauduit_test:armoires-fo")
-		assertTrue(fo != null)
-		assertTrue(fo.styles.size == 1)
-		assertTrue(capInfo.getLayersCount() == 29)
-		// grep -A1 '<Style' src/test/resources/fr/beneth/wxslib/operations/sdi-geor.getcap.xml | grep Name | sort -u | wc -l
-		assertTrue(capInfo.getStylesCount() == 17)
-	}
-	
-	@Test
-	void testOpaqueQueryableLayer() {
-		def getCapResp = this.getClass().getResource("sdi-geor.getcap.xml")
-		def capInfo = Capabilities.mapFromDocument(getCapResp.toString())
-		def gshhsLayer = capInfo.findLayerByName("gshhs:GSHHS_l_L1")
-		def mainLayer = (Layer) capInfo.layers.first()
-		assertFalse(gshhsLayer.opaque)
-		assertTrue(gshhsLayer.queryable)
-		assertFalse(mainLayer.opaque)
-		assertFalse(mainLayer.queryable)
-	}
-    
+    @Test
+    void testSdiGeorchestraOrgWmsCapabilities() {
+        def getCapResp = this.getClass().getResource("sdi-geor.getcap.xml")
+        def capInfo = Capabilities.mapFromDocument(getCapResp.toString())
+
+        assertTrue(capInfo.name == "WMS")
+        assertTrue(capInfo._abstract ==~ /.*modular and opensource spatial data infrastructure.*/)
+        assertTrue(capInfo.keywords.size == 2)
+        assertTrue(capInfo.layers.size == 1)
+        assertTrue(capInfo.title == "geOrchestra Web Map Service")
+        assertTrue(capInfo.contactEmail == "psc@georchestra.org")
+        assertTrue(capInfo.contact == "geOrchestra PSC")
+
+        Layer fo = capInfo.findLayerByName("pmauduit_test:armoires-fo")
+        assertTrue(fo != null)
+        assertTrue(fo.styles.size == 1)
+        assertTrue(capInfo.getLayersCount() == 29)
+        // grep -A1 '<Style' src/test/resources/fr/beneth/wxslib/operations/sdi-geor.getcap.xml | grep Name | sort -u | wc -l
+        assertTrue(capInfo.getStylesCount() == 17)
+    }
+
+    @Test
+    void testOpaqueQueryableLayer() {
+        def getCapResp = this.getClass().getResource("sdi-geor.getcap.xml")
+        def capInfo = Capabilities.mapFromDocument(getCapResp.toString())
+        def gshhsLayer = capInfo.findLayerByName("gshhs:GSHHS_l_L1")
+        def mainLayer = (Layer) capInfo.layers.first()
+        assertFalse(gshhsLayer.opaque)
+        assertTrue(gshhsLayer.queryable)
+        assertFalse(mainLayer.opaque)
+        assertFalse(mainLayer.queryable)
+    }
+
     @Test
     void testFormat() {
         def getCapResp = this.getClass().getResource("sdi-geor.getcap.xml")
         def capInfo = Capabilities.mapFromDocument(getCapResp.toString())
         def gm = capInfo.capabilityRequests.find { it.name.toLowerCase() == "getmap" }
-        
+
         assertTrue(gm != null)
         def imagePngFound = gm.formats.find { it == "image/png" } != null
         assertTrue(imagePngFound)
@@ -66,13 +66,12 @@ class CapabilitiesTest {
         def getCapResp = this.getClass().getResource("sdi-geor.getcap.xml")
         def capInfo = Capabilities.mapFromDocument(getCapResp.toString())
         def gshhsLayer = capInfo.findLayerByName("gshhs:GSHHS_l_L1")
-        
+
         assertTrue(gshhsLayer.boundingBoxes.size() == 5)
         assertTrue(gshhsLayer.boundingBoxes.find { it.crs == "EPSG:4326" } != null)
         assertTrue(capInfo.layers.first().boundingBoxes.size() == 5)
         assertTrue(capInfo.layers.first().boundingBoxes.find { it.crs == "EPSG:4326" } != null)
         assertTrue(capInfo.layers.first().boundingBoxes.find { it.crs == "EPSG:4326" }.miny == "-180.0" )
         assertTrue(capInfo.layers.first().boundingBoxes.find { it.crs == "EPSG:4326" }.maxx == "90.0" )
-        
     }
 }
