1668ab42f99bb67f20cf75314a4abcc1cebe1731
Pierre Mauduit <pierre.mauduit@gmail.com>
2015-11-07 11:30:16 +0100

    removing package-info
    Makes the compilation fail.

--- src/main/groovy/fr/beneth/cswlib/geonetwork/package-info.groovy
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains extra classes to fetch informations
- * from a GeoNetwork node. It uses specific / "proprietary" webservices
- * that a regular CSW does not provide.
- * 
- * @author pmauduit
- */
-package fr.beneth.cswlib.geonetwork
