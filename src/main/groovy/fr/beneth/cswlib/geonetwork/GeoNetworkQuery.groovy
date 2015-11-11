a4d898dbf2b580be4e4aaf5e3bd96c1324df4952
Pierre Mauduit <pierre.mauduit@gmail.com>
2015-11-11 18:11:38 +0100

    Geonetwork-specific - Adding a query (q?...) parser

---
+++ src/main/groovy/fr/beneth/cswlib/geonetwork/GeoNetworkQuery.groovy
@@ -0,0 +1,48 @@
+package fr.beneth.cswlib.geonetwork
+
+import fr.beneth.cswlib.metadata.Metadata
+
+/**
+ * This class is a wrapper to the classic querying interface
+ * of GeoNetwork ("/srv/eng/q?").
+ *
+ * @author pmauduit
+ *
+ */
+class GeoNetworkQuery {
+
+    ArrayList<Metadata> metadatas = new ArrayList<Metadata>()
+    
+    public static GeoNetworkQuery mapFromUrl(String url) {
+        def gnq = new GeoNetworkQuery()
+        
+        def gnHost = url
+        def patGnHost = url =~ /(http(s?):\/\/.*\/geonetwork\/).*$/
+        if (patGnHost.matches()) {
+            gnHost = patGnHost[0][1]
+        }
+
+        def xml = new XmlSlurper().parse(url)
+        xml.declareNamespace("geonet": "http://www.fao.org/geonetwork")
+        xml.'metadata'.each {
+                def newMd = new Metadata()
+                it.'keyword'.each { kw -> newMd.keywords << kw.text() }
+                newMd.title = it.'title'.text()
+                newMd._abstract = it.'abstract'.text()
+                def graphicOverview = it.'image'.text()
+                if (graphicOverview) {
+                    def splitted = graphicOverview.split("\\|")
+                    if (splitted[1]) {
+                        if (splitted[1].startsWith("../../"))
+                            splitted[1] = splitted[1].replace("../../", gnHost)
+                        newMd.graphicOverviewUrls << splitted[1]
+                    }
+                    
+                }
+                newMd.fileIdentifier = it.'geonet:info'.'uuid'.text()
+                newMd.scopeCode = it.'type'.text()
+                gnq.metadatas << newMd
+        }
+        return gnq
+    }
+}
---
+++ src/test/groovy/fr/beneth/cswlib/geonetwork/GeoNetworkQueryTest.groovy
@@ -0,0 +1,30 @@
+package fr.beneth.cswlib.geonetwork
+
+import static org.junit.Assert.*
+import static org.junit.Assume.assumeTrue
+
+import org.junit.Test
+
+class GeoNetworkQueryTest {
+
+    @Test
+    public void geonetworkQueryTest() {
+        URL fixture = this.getClass().getResource("query-sdi.xml")
+        assumeTrue(fixture != null)
+                
+        GeoNetworkQuery q = GeoNetworkQuery.mapFromUrl(fixture.toString())
+        assertTrue(q.metadatas.size() == 32)
+        def myMd = q.metadatas.find { it.title == "Recensement des armoires de rues du réseau de "+
+            "fibres optiques sur l'agglomération de Chambéry"}
+        assertTrue(myMd != null)
+        assertTrue(myMd.fileIdentifier != "")
+        assertTrue(myMd.keywords.size()  == 3)
+        assertTrue(myMd.scopeCode != "")
+        assertTrue(myMd._abstract != "")
+        
+        
+        
+        
+    }
+
+}
