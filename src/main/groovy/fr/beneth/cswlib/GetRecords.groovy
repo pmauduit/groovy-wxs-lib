5d84781a918024c8b6c640e0db237f61b1e19cc7
Pierre Mauduit <pierre.mauduit@gmail.com>
2015-12-04 21:16:11 +0100

    CSW get records parsing
    Putting things together, still in a WIP state

--- src/main/groovy/fr/beneth/cswlib/GetRecords.groovy
+++ src/main/groovy/fr/beneth/cswlib/GetRecords.groovy
@@ -1,9 +1,12 @@
 package fr.beneth.cswlib
 
+import org.apache.http.HttpResponse;
+
 import fr.beneth.cswlib.metadata.Metadata
 import groovy.util.slurpersupport.NodeChild
 import groovy.xml.MarkupBuilder
 import groovy.xml.StreamingMarkupBuilder
+import groovy.xml.XmlUtil;
 import groovyx.net.http.HTTPBuilder
 import groovyx.net.http.ContentType
 import groovyx.net.http.Method
@@ -16,9 +19,10 @@ class GetRecords {
     /* pagination utils */
     private def maxRecords = 10, currentPosition = 0
 
+    // GeoNetwork-specific:
     // Possible to search for local MD using the _isHarvested field
     // (even if not promoted in the GetCapabilities)
-    
+
     public static String buildQuery(int startPosition, String type) {
         return """<?xml version="1.0"?>
        <csw:GetRecords xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" xmlns:ogc="http://www.opengis.net/ogc"
@@ -46,21 +50,27 @@ class GetRecords {
         
     }
     
-    private void parseResponse(def resp) {
-        println resp
-    }
+
 
     public static GetRecords getAllDatasetMetadatas(String url) {
         def http = new HTTPBuilder(url)
         def ret = new GetRecords()
-        def finished = true
+        def done = false
 
-        while (finished) {
-            http.post(body: buildQuery(1, "dataset"), requestContentType: ContentType.XML) { resp ->
-                ret.parseResponse(resp)
-                finished = false
+        int currentIdx = 1
+        while (! done) {
+            http.post(body: buildQuery(currentIdx, "dataset"), requestContentType: ContentType.XML) { resp ->
+                def response = new XmlSlurper().parseText(resp.entity.content.text)
+                .declareNamespace(csw: "http://www.opengis.net/cat/csw/2.0.2")
+                println XmlUtil.serialize(response)
+                int recordsMatched = Integer.parseInt(response.'csw:SearchResults'.'@numberOfRecordsMatched'.toString())
+                int nextRecord = Integer.parseInt(response.'csw:SearchResults'.'@nextRecord'.toString())
+                // TODO: parsing logic here
+                if (nextRecord > recordsMatched)
+                    done = true
+                else
+                    currentIdx = nextRecord
             }
-            
         }
         return ret
     }
